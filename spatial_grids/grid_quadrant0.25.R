19bc61ede39da07e118655b11b88d71315a0be40
Kotaro Ono <kotaro.ono@hi.no>
2023-05-02 18:19:19 +0200

    Update grid_quadrant0.25.R
    generalized it

--- spatial_grids/grid_quadrant0.25.R
+++ spatial_grids/grid_quadrant0.25.R
@@ -1,21 +1,23 @@
 
-csquare_grid =  CSquare( csquare_centroid$X, csquare_centroid$Y,  0.5 )
+parentgridlevel = 0.5
+finalgrid_level = 0.25
+
+csquare_grid =  CSquare( csquare_centroid$X,csquare_centroid$Y,parentgridlevel)
 
 ## piece of code to hack for the special case of 0.25degree
-add_quadrant <- function(lon,lat){
-  val1 <- trunc((lon %% 0.25 ) *2 /0.25)
-  val2 <- trunc((lat %% 0.25 ) *2 /0.25)
-  quadrant_id <- function(x,y){
-    val = ifelse((x ==0 & y==0), "_1",
-                 ifelse((x ==1 & y==0), "_2",
-                        ifelse((x ==0 & y==1), "_3",
-                               "_4")))
+add_quadrant <- function(lon, lat, parent_grid, kid_grid){
+  lon_id <- 1+trunc((lon %% parent_grid )/kid_grid)
+  lat_id <- parent_grid/kid_grid - trunc((lat %% parent_grid )/kid_grid)
+  quadrant_id <- function(x,y,parent_grid,kid_grid){
+    val = paste0("_", (y-1) * parent_grid/kid_grid + x)
     return(val)
   }
-  out <- sapply(1:length(lon), function(x) quadrant_id(val1[x],val2[x]))
+  out <- sapply(1:length(lon), function(x) quadrant_id(lon_id[x],lat_id[x],parentgridlevel,finalgrid_level))
   return(out)
 }
 
-test <- add_quadrant(lon=csquare_centroid$X, lat=csquare_centroid$Y)
+test <- add_quadrant(lon=csquare_centroid$X, lat=csquare_centroid$Y, parent_grid = 0.5, kid_grid=0.25)
 
 csquare_grid_new <- sapply(1:length(csquare_grid), function(x) paste0(csquare_grid[x], test[x]))
+
+head(data.frame(lon=csquare_centroid$X, lat=csquare_centroid$Y, ID=csquare_grid, ID_new=csquare_grid_new))
