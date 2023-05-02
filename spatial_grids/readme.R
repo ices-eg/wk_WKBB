a06d9681f6b7a9bedf74cfbd7864e278ee5c42f7
Roi Martinez <roimarful@gmail.com>
2023-05-02 11:25:53 +0100

    Create readme.R

---
+++ spatial_grids/readme.R
@@ -0,0 +1,14 @@
+###### Spatial Framework for WKBB
+
+## 1. Define spatial grid extent and size fo grid cells
+  ## List of grids to be explored: 
+    - Seabirds grids perspective: 
+      - 0.25
+      - 0.5
+      - 1
+      - 5
+    - Fisheries perspective: 
+      - 0.05 
+      - 0.01 ( optional ) 
+      
+## 2. 
