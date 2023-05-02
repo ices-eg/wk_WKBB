2100180e1ac9770020f7caa4018e50d1e2b7ac9e
Roi Martinez <roimarful@gmail.com>
2023-05-02 14:53:36 +0100

    Update readme.R

--- spatial_grids/readme.R
+++ spatial_grids/readme.R
@@ -1,8 +1,8 @@
 ###### Spatial Framework for WKBB
 
 ## 1. Define spatial grid extent and size fo grid cells
-  ## List of grids to be explored: 
-    - Seabirds grids perspective: 
+  ## List of grids cell size  to be explored: 
+    - Seabirds perspective: 
       - 0.25
       - 0.5
       - 1
@@ -10,5 +10,15 @@
     - Fisheries perspective: 
       - 0.05 
       - 0.01 ( optional ) 
+  - Common resolution: 
+      - 0.25
       
-## 2. 
+## 2.Spatial resolution  
+
+  - Monthly aggregation 
+
+##3 . Fleet segment resolution
+  
+  - All activity 
+  - Active/passive
+  - Pelagic/demersal 
