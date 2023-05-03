af6c00fd87488f1780c2dc1b64a2b87b2a60f3c6
mariazti <40487850+mariazti@users.noreply.github.com>
2023-05-03 18:05:25 +0200

    Update readme.R

--- spatial_grids/readme.R
+++ spatial_grids/readme.R
@@ -23,7 +23,14 @@
   - All gears 
   - Active/passive
   - Pelagic/demersal
-  - Gears: Otrwler, liners, gillnets, MIS
+  - Gears: Trawler, liners, gillnets, MIS (desirible buut not possible with current data)
+  	- based on available data the categories:
+		- bottom trawling (OTB, OTT, area 3 and green area 1 in MIS) | active | demersal | (OT ByRa category)
+		- pelagic trawl (OTM, most of MIS in area 2)  | active  | pelagic | (OT ByRa category)
+		- liners (LX ) | passive | demersal 
+		- nets (GTR ) | passive | demersal
+		- seiners (PS ) | passive | pelagic
+		- pots and traps (FPO) | passive 
 
 ## 4. Activity indicator
   
@@ -34,10 +41,10 @@
 ## 5. FISHERIES DATA PRODUCTS
 
   ## 5.1.1. Data product name: fish_2021_area2_0_25_year_all 
-    - Special extent: Area 2
+    - Special extent: All areas
     - Spatial resolution: 0.25
-    - Temporal resolution: year
-    - Fleet resolution: all gears 
+    - Temporal resolution: by year, month
+    - Fleet resolution: by gears 
     - Activity indicator: nb vessel, fishing days by vessel, total fishing days
 
   ## 5.1.2. Data product name: fish_2_2021_area2_0_25_month_all 
@@ -45,5 +52,5 @@
     - Spatial resolution: 0.25
     - Temporal resolution: month
     - Fleet resolution: by gear
-    - Activity indicator: nb vessel, fishing days by vessel, total fishing days
+    - Activity indicator: nb vessel, fishing days by vessel, total fishing days -> mean()
 	
