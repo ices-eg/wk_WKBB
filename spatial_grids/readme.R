99a7690a7ebe42461db81cd91eafb96af930cb39
mariazti <40487850+mariazti@users.noreply.github.com>
2023-05-04 12:17:28 +0200

    Update readme.R

--- spatial_grids/readme.R
+++ spatial_grids/readme.R
@@ -25,12 +25,13 @@
   - Pelagic/demersal
   - Gears: Trawler, liners, gillnets, MIS (desirible buut not possible with current data)
   	- based on available data the categories:
-		- bottom trawling (OTB, OTT, area 3 and green area 1 in MIS) | active | demersal | (OT ByRa category)
-		- pelagic trawl (OTM, most of MIS in area 2)  | active  | pelagic | (OT ByRa category)
-		- liners (LX ) | passive | demersal 
-		- nets (GTR ) | passive | demersal
-		- seiners (PS ) | passive | pelagic
-		- pots and traps (FPO) | passive 
+		- bottom trawling (OTB, OTT, area 3 and green area 1 in MIS) | active | demersal | (trawl ByRas category)
+		- pelagic trawl (OTM, most of MIS in area 2)  | active  | pelagic | (trawl ByRas category)
+		- liners (LX ) | passive | demersal | (lines ByRas category)
+		- nets (GTR ) | passive | demersal | (net ByRas category)
+		- seiners (PS ) | passive | pelagic | (seiner ByRas category)
+		- pots and traps (FPO) | passive | (trap ByRas category)
+		- Miscellaneous (MIS) | unknown | (UNK ByRas category)
 
 ## 4. Activity indicator
   
@@ -40,17 +41,38 @@
 
 ## 5. FISHERIES DATA PRODUCTS
 
-  ## 5.1.1. Data product name: fish_2021_area2_0_25_year_all 
+  ## 5.1.1. Data product name: fish_1_1_2018_2022_neafc_0_25_year_month_gear_all_ind 
     - Special extent: All areas
     - Spatial resolution: 0.25
     - Temporal resolution: by year, month
-    - Fleet resolution: by gears 
-    - Activity indicator: nb vessel, fishing days by vessel, total fishing days
+    - Fleet resolution: by gears (level 4)
+    - Activity indicator: nb vessel, fishing days by vessel, total fishing days, days of the month ratio
 
-  ## 5.1.2. Data product name: fish_2_2021_area2_0_25_month_all 
-    - Special extent: Area 2
+  ## 5.1.2. Data product name: fish_1_2_2018_2022_neafc_0_25_month_gear_days_ratio 
+    - Special extent: All areas
     - Spatial resolution: 0.25
     - Temporal resolution: month
-    - Fleet resolution: by gear
-    - Activity indicator: nb vessel, fishing days by vessel, total fishing days -> mean()
-	
+    - Fleet resolution: by gear (level 4)
+    - Activity indicator: days of the month ratio -> mean()
+
+  ## 5.1.3. Data product name: fish_1_3_2018_2022_neafc_0_25_month_gear_tot_days 
+    - Special extent: All areas
+    - Spatial resolution: 0.25
+    - Temporal resolution: month
+    - Fleet resolution: by gear (level 4)
+    - Activity indicator: total fishing days -> mean()
+    
+  ## 5.2.1. Data product name: fish_1_1_2018_2022_neafc_0_25_year_month_byrasgear_all_ind 
+    - Special extent: All areas
+    - Spatial resolution: 0.25
+    - Temporal resolution: by year, month
+    - Fleet resolution: by gear (ByRas category)
+    - Activity indicator: nb vessel, fishing days by vessel, total fishing days, days of the month ratio
+   
+  ## 5.2.2. Data product name: fish_2_2_2018_2022_neafc_0_25_month_byrasgear_tot_days  
+    - Special extent: All areas
+    - Spatial resolution: 0.25
+    - Temporal resolution: month
+    - Fleet resolution: by gear (ByRas category)
+    - Activity indicator: total fishing days -> mean()
+       
